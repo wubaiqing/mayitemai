edf1c8b7cf458750430a51b1dda737baf5845b64
wubaiqing <wubaiqing@vip.qq.com>
2015-04-24 16:18:39 +0800

    提交错误显示

--- app/models/brand.rb
+++ app/models/brand.rb
@@ -3,13 +3,26 @@ class Brand
   include Mongoid::Timestamps
   include Mongoid::BaseModel
 
-  validates :name, presence: true
+  validates :name, :wangwang, :banner_picture, :recommend_picture, presence: true
 
   field :name
   field :wangwang
   field :banner_picture
   field :recommend_picture
-  field :publish, type: Mongoid::Boolean, default: false
+  field :state, type: Integer, default: 0
+
+  STATE = {
+    # 软删除
+    deleted: -1,
+    # 正常
+    normal: 1,
+    # 屏蔽
+    blocked: 2,
+  }
+
+  def attributes
+    { 'name' => @name }
+  end
 
   before_save :auto_set_value
   def auto_set_value
