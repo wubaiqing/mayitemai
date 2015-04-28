1c86a3551192d45e377990019c5adf394d29c8a8
wubaiqing <wubaiqing@vip.qq.com>
2015-04-29 01:38:59 +0800

    添加商品

--- app/models/good.rb
+++ app/models/good.rb
@@ -1,3 +1,15 @@
 class Good
   include Mongoid::Document
+  include Mongoid::Timestamps
+  include Mongoid::BaseModel
+
+  field :taobao_id, type: Integer, defualt: 0
+  field :taobao_url
+  field :title
+  field :original_price
+  field :price
+  field :picture_url
+  field :sort, type: Integer, defualt: 0
+  field :brand_id, type: Integer, defualt: 0
+
 end
