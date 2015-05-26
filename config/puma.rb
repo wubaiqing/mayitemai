425e978216d9e35518e4574a571a3e249252760e
wubaiqing <wubaiqing@vip.qq.com>
2015-05-27 00:53:47 +0800

    added puma

--- config/puma.rb
+++ config/puma.rb
@@ -1,6 +1,8 @@
 APP_ROOT = '/home/deploy/mayitemai'
 pidfile "#{APP_ROOT}/tmp/pids/puma.pid"
 state_path "#{APP_ROOT}/tmp/pids/puma.state"
+bind 'unix://tmp/sockets/puma.sock'
+activate_control_app 'unix://tmp/sockets/pumactl.sock'
 daemonize true
 workers 2
 threads 4,8
