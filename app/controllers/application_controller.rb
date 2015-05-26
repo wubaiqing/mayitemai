608eecaacbcf8687ddfdcf62815fb14a9c0e1d74
wubaiqing <wubaiqing@vip.qq.com>
2015-05-26 20:10:48 +0800

    修改过滤

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,8 +3,6 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  before_filter :require_user
-
   def require_user
     if current_user.blank?
       respond_to do |format|
