21bb8f9ad0e402eaf312b7920480f311ea072616
wubaiqing <wubaiqing@vip.qq.com>
2015-06-06 19:39:10 +0800

    添加admin

--- config/routes.rb
+++ config/routes.rb
@@ -1,27 +1,42 @@
 Rails.application.routes.draw do
 
-  resources :details
-  devise_for :users
+
+  # 用户系统
+  devise_for :users, :path => 'accounts'
 
   # 关于我们
   get '/document/about', to: 'document#about'
+
+  # 联系我们
   get '/document/contact', to: 'document#contact'
+
+  # 商户合作
   get '/document/supplier', to: 'document#supplier'
 
-  # 获取淘宝商品
-  resources :goods do
-    collection do
-      get 'fetch_taobao_repositories'
-    end
-  end
+  # 详情页
+  resources :details
 
-  # 协议搜索
-  resources :brands do
-    collection do
-      get 'search'
-    end
-  end
 
+  namespace :admin do
+
+    # 获取淘宝商品
+    resources :goods do
+      collection do
+        get 'fetch_taobao_repositories'
+      end
+    end
+
+    # 协议搜索
+    resources :brands do
+      collection do
+        get 'search'
+      end
+    end
+
+    resources :brands, :goods, :homes, :photos
+
+    root 'homes#index'
+  end
 
 
   resources :brands, :goods, :homes, :photos
