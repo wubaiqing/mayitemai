4e3bf02e7a0e1aade0bfe478a2e9f09574d0e66a
wubaiqing <wubaiqing@vip.qq.com>
2015-06-08 18:30:48 +0800

    删除test文件

--- test/controllers/document_controller_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class DocumentControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
--- test/models/cpanel/cate_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class Cpanel::CateTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
