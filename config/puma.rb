32bf8d714722158d1bed05ff529957d701d2fb81
wubaiqing <wubaiqing@vip.qq.com>
2015-06-09 17:44:09 +0800

    修改后台地址

--- config/puma.rb
+++ config/puma.rb
@@ -1,6 +1,5 @@
-APP_ROOT = '/home/deploy/mayitemai'
-pidfile "#{APP_ROOT}/tmp/pids/puma.pid"
-state_path "#{APP_ROOT}/tmp/pids/puma.state"
+pidfile 'tmp/pids/puma.pid'
+state_path 'tmp/pids/puma.state'
 bind 'unix://tmp/sockets/puma.sock'
 activate_control_app 'unix://tmp/sockets/pumactl.sock'
 daemonize true
