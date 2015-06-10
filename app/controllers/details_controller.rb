d84cdbc286b813f871a923ba8e556e38f3a91294
wubaiqing <wubaiqing@vip.qq.com>
2015-06-10 22:05:52 +0800

    删除控制器

--- app/controllers/details_controller.rb
+++ app/controllers/details_controller.rb
@@ -6,14 +6,6 @@ class DetailsController < ApplicationController
     @goods = Good.find_by_wangwang(@brand.wangwang).desc(:id)
   end
 
-  def destroy
-    @detail.destroy
-    respond_to do |format|
-      format.html { redirect_to details_url, notice: 'Detail was successfully destroyed.' }
-      format.json { head :no_content }
-    end
-  end
-
   private
     def set_detail
       @brand = Brand.find(params[:id])
