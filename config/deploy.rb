846becdb9f2442424314ee71c8ca81d0727ca2bf
wubaiqing <wubaiqing@vip.qq.com>
2015-06-14 17:35:04 +0800

    delete deploy files

--- config/deploy.rb
+++ config/deploy.rb
@@ -8,7 +8,7 @@ set :repo_url, 'https://github.com/wubaiqing/mayitemai.git'
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
 # Default deploy_to directory is /var/www/my_app_name
-set :deploy_to, '/home/deploy/mayitemai/current'
+set :deploy_to, "/data/www/#{fetch(:application)}"
 
 # Default value for :scm is :git
 set :scm, :git
@@ -26,7 +26,7 @@ set :scm, :git
 # set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml')
 
 # Default value for linked_dirs is []
-# set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
+set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets')
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
