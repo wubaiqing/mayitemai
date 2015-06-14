6ff4f0846f343f49f14407d5a22cc1effcb552fe
wubaiqing <wubaiqing@vip.qq.com>
2015-06-14 19:35:46 +0800

    开启两个puma进程

--- config/deploy.rb
+++ config/deploy.rb
@@ -48,7 +48,7 @@ set :puma_error_log, "#{shared_path}/log/puma_error.log"
 set :puma_role, :app
 set :puma_env, fetch(:rack_env, fetch(:rails_env, 'production'))
 set :puma_threads, [0, 16]
-set :puma_workers, 0
+set :puma_workers, 2
 set :puma_worker_timeout, nil
 set :puma_init_active_record, false
 set :puma_preload_app, true
