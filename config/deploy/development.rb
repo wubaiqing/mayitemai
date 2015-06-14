3994344789aa3c1a3bef7622fd485039b858ec55
wubaiqing <wubaiqing@vip.qq.com>
2015-06-14 22:44:32 +0800

    部署puma

--- config/deploy/development.rb
+++ config/deploy/development.rb
@@ -3,6 +3,11 @@
 # Defines a single server with a list of roles and multiple properties.
 # You can define all roles on a single server, or split them:
 
+require 'puma/capistrano'
+
+set :application, "mayitemai"
+set :puma_config_file, "config/puma.rb"
+
 server '192.168.8.8', user: 'vagrant', roles: %w{app db web}, my_property: :my_value
 # server 'example.com', user: 'deploy', roles: %w{app web}, other_property: :other_value
 # server 'db.example.com', user: 'deploy', roles: %w{db}
