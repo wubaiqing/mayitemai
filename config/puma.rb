98c186afa63b3ae05d37739e91af674c385ee3be
wubaiqing <wubaiqing@vip.qq.com>
2015-06-14 21:10:51 +0800

    修改puma配置

--- config/puma.rb
+++ config/puma.rb
@@ -1,8 +1,25 @@
-pidfile 'tmp/pids/puma.pid'
-state_path 'tmp/pids/puma.state'
-bind 'unix://tmp/sockets/puma.sock'
-activate_control_app 'unix://tmp/sockets/pumactl.sock'
-daemonize true
+#!/usr/bin/env puma
+
+directory '/data/www/mayitemai/current'
+rackup "/data/www/mayitemai/current/config.ru"
+environment 'production'
+
+pidfile "/data/www/mayitemai/shared/tmp/pids/puma.pid"
+state_path "/data/www/mayitemai/shared/tmp/pids/puma.state"
+stdout_redirect '/data/www/mayitemai/shared/log/puma_access.log', '/data/www/mayitemai/shared/log/puma_error.log', true
+
+
+threads 0,16
+
+bind 'unix:///data/www/mayitemai/shared/tmp/sockets/puma.sock'
+
 workers 2
-threads 4,8
+
+
+
 preload_app!
+
+on_restart do
+  puts 'Refreshing Gemfile'
+  ENV["BUNDLE_GEMFILE"] = "/data/www/mayitemai/current/Gemfile"
+end
