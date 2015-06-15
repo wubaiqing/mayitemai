dee4b1d65baf1aa54a9f727dfeee56419b20f647
wubaiqing <wubaiqing@vip.qq.com>
2015-06-15 22:13:06 +0800

    Added rvm path

--- config/deploy.rb
+++ config/deploy.rb
@@ -42,10 +42,10 @@ namespace :deploy do
   after :restart, :clear_cache do
     on roles(:web), in: :groups, limit: 3, wait: 10 do
       # Here we can do anything such as:
-      within release_path do
-        execute :rake, 'cache:clear'
-        execute :rake, 'assets:precompile'
-      end
+      # within release_path do
+      #   execute :rake, 'cache:clear'
+      #   execute :rake, 'assets:precompile'
+      # end
     end
   end
 
