36fce0b122625e059d035e59dca02ae607e422ce
wubaiqing <wubaiqing@vip.qq.com>
2015-06-21 00:10:25 +0800

    新增七牛配置KEY配置文件

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,12 @@
+# coding: utf-8
+
+# 应用控制器
 class ApplicationController < ActionController::Base
+
+  # CSRF
   protect_from_forgery with: :exception
 
+  # 设置meta方法
   def set_seo_meta(title = '', meta_keywords = '', meta_description = '')
     @page_title = "#{title}" if title.length > 0
     @meta_keywords = meta_keywords
