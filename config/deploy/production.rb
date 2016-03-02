30705ab64c403dffb192f4ffcf8f167c3941a387
wubaiqing <wubaiqing@vip.qq.com>
2016-03-02 19:07:32 +0800

    修改服务器IP

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -3,7 +3,7 @@
 # Defines a single server with a list of roles and multiple properties.
 # You can define all roles on a single server, or split them:
 
-server '120.26.208.136', user: 'deploy', roles: %w{app db web}, my_property: :my_value, port: 22
+server '123.56.194.49', user: 'deploy', roles: %w{app db web}, my_property: :my_value, port: 22
 # server 'example.com', user: 'deploy', roles: %w{app web}, other_property: :other_value
 # server 'db.example.com', user: 'deploy', roles: %w{db}
 
